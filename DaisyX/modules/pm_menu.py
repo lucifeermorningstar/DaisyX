a970c3b22fabc67306f79327327fb16df737eef4
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-03-31 13:09:18 +0530

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -73,10 +73,11 @@ async def get_start_func(message, strings, edit=False):
     buttons = InlineKeyboardMarkup()
     buttons.add(InlineKeyboardButton(strings['btn_help'], callback_data='get_help'))
     buttons.add (InlineKeyboardButton(strings['btn_lang'], callback_data='lang_btn'),
-                 InlineKeyboardButton(strings['btn_source'], url='https://github.com/TeamDaisyX/'))
-    buttons.add(InlineKeyboardButton(strings['btn_channel'], url='https://t.me/DaisyXUpdates'),
-                InlineKeyboardButton('👥 Support Grup', url='https://t.me/DaisyXUpdates'))
-    buttons.add(InlineKeyboardButton("👸🏼 Add DaisyX to your group", url=f'https://telegram.me/daisyxbot?startgroup=true'))
+                 InlineKeyboardButton(strings['btn_source'], url='https://github.com/TEAMDAISYXDEVS/'))
+    buttons.add(InlineKeyboardButton(strings['btn_channel'], url='https://t.me/DaisyXNews'),
+                InlineKeyboardButton('👥 Support Group', url='https://t.me/DaisyXSupport')
+                InlineKeyboardButton('🇮🇳 Dev', url='https://t.me/lucifeermorningstar'))
+    buttons.add(InlineKeyboardButton("👸🏼 Add DaisyX to your group", url=f'https://telegram.me/daisyx_bot?startgroup=true'))
     # Handle error when user click the button 2 or more times simultaneously
     with suppress(MessageNotModified):
         await task(strings['start_hi'], reply_markup=buttons)
@@ -118,7 +119,7 @@ async def help_cmd(message, strings):
 async def help_cmd_g(message, strings):
     text = (strings['btn_group_help'])
     button = InlineKeyboardMarkup().add(InlineKeyboardButton(
-        text=text, url="https://t.me/DaisyXBOT?start"))
+        text=text, url="https://t.me/DaisyX_BOT?start"))
     await message.reply(strings['help_header'], reply_markup=button)
 
 
